8ea14027e1dee540bd6610c71d0ad1fef94dea33
reesharper <reestimharper@gmail.com>
2020-12-19 17:14:22 -0500

    added axiosWithAuth file for authentication as well as protected route structure.

---
+++ src/utils/axiosAuth.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const axiosWithAuth = () => {
+
+  const token = localStorage.getItem('token');
+
+  return axios.create({
+    baseURL: 'http://localhost5000/api',
+    headers: {
+      authorization: token
+    }
+  })
+}
+
+export default axiosWithAuth;
