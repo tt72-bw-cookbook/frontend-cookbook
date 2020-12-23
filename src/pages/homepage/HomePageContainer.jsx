aa62c226fcd00a0c7ff5b5927973a804ce851252
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-12-22 21:56:19 -0600

    add axiosSecret

--- src/pages/homepage/HomePageContainer.jsx
+++ src/pages/homepage/HomePageContainer.jsx
@@ -1,12 +1,56 @@
+import React, { useState } from "react";
 import styled from "styled-components";
 import components from "../../common/components/";
+import axiosAuth, { axiosSecret } from "../../utils/axiosAuth";
+
 const { Header } = components;
 
 const HomePageContainer = props => {
+	const [v, setV] = useState({
+		username: "",
+		password: "",
+		primaryemail: "",
+	});
+
+	const handleChange = e => {
+		const { name, value } = e.target;
+		setV({ ...v, [name]: value });
+	}
+
+	/** 
+	 * here's an example of a call to axiosSecret.
+	 * realistically, it'll look more like so:
+	 * axiosSecret()
+	 * 	.post("login", "grant_type=password&username=${submittedUsername}&password=${submittedPassword}")
+	 * .then(res => console.log(res.data.access_token))
+	 * .catch(err => console.log("it's not my fault"));
+	 * 
+	 * 
+	 * res.data.access_token will hold the ACTUAL ACCESS TOKEN NECESSARY FOR ANY AUTHORIZED ENDPOINTS
+	 * 
+	 */
+	const handleSubmit = e => {
+		e.preventDefault();
+		axiosSecret().post("login", "grant_type=password&username=chaz&password=password")
+			.then(res => {
+				console.log(res, res.data, res.data.auth_token)
+				console.log(res.data.access_token)
+			})
+			.catch(err => console.log(err));
+	}
+
 	return (
 		<div>
 			<Header />
 			<WelcomeHeading> HOME PAGE </WelcomeHeading>
+			<div>
+				<form onSubmit={handleSubmit}>
+					<input type="text" name="username" value={v.username} onChange={handleChange} />
+					<input type="text" name="password" value={v.password} onChange={handleChange} />
+					<input type="text" name="primaryemail" value={v.primaryemail} onChange={handleChange} />
+					<button>submit</button>
+				</form>
+			</div>
 		</div>
 	);
 }
--- src/utils/axiosAuth.js
+++ src/utils/axiosAuth.js
@@ -1,15 +1,38 @@
 import axios from 'axios';
 
-const axiosWithAuth = () => {
+const axiosAuth = () => {
+	const token = localStorage.getItem('token');
 
-  const token = localStorage.getItem('token');
-
-  return axios.create({
-    baseURL: 'http://localhost5000/api',
-    headers: {
-      authorization: token
-    }
-  })
+	return axios.create({
+		headers: {
+			Authorization: "Bearer " + token,
+			"Content-Type": "application/json"
+		},
+		baseURL: 'http://localhost:2019/'
+	})
 }
 
-export default axiosWithAuth;
+/* axiosSecret is for LOGIN ONLY 
+* this will not work if you're trying to send an auth token.
+* this is only applicable for __previously registered users__ who are 
+* simply trying to log in once again. 
+
+* only for use of REGISTERED USERS who need to gain an AUTH TOKEN again
+
+*/
+export const axiosSecret = () => {
+	const clientID = "AndIKnewExactlyWhatToDo";
+	const clientSecret = "ButInAMuchMoreRealSenseIHadNoIdeaWhatToDo";
+
+	const auth = window.btoa(clientID + ":" + clientSecret);
+
+	return axios.create({
+		headers: {
+			Authorization: 'Basic ' + auth,
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
+		baseURL: "http://localhost:2019/"
+	})
+}
+
+export default axiosAuth;
