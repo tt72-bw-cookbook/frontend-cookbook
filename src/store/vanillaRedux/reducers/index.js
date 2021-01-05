000729bb6f09978941f7f3841a88ff8778142d77
reesharper <reestimharper@gmail.com>
2021-01-05 03:43:40 -0500

    added every singe action/reducer for each endpoint

--- src/store/vanillaRedux/reducers/index.js
+++ src/store/vanillaRedux/reducers/index.js
@@ -5,13 +5,29 @@ import {
   FETCH_CURRENT_USER_RECIPES_START,
   FETCH_CURRENT_USER_RECIPES_SUCCESS,
   FETCH_CURRENT_USER_RECIPES_FAILURE,
-
+  POST_USER_LOGIN_START,
+  POST_USER_LOGIN_SUCCESS,
+  POST_USER_LOGIN_FAILURE,
+  FETCH_USER_LOGOUT_START,
+  FETCH_USER_LOGOUT_SUCCESS,
+  FETCH_USER_LOGOUT_FAILURE,
+  POST_USER_RECIPE_START,
+  POST_USER_RECIPE_SUCCESS,
+  POST_USER_RECIPE_FAILURE,
+  DELETE_RECIPE_BY_ID_START,
+  DELETE_RECIPE_BY_ID_SUCCESS,
+  DELETE_RECIPE_BY_ID_FAILURE,
+  PUT_RECIPE_BY_ID_START,
+  PUT_RECIPE_BY_ID_SUCCESS,
+  PUT_RECIPE_BY_ID_FAILURE
 } from '../actions/index'
 
 const initialState = {
   isLoading: false,
+  isLoggedIn: false,
   recipeData: [],
   userData: {},
+  newRecipeData: {},
   error:''
 }
 
@@ -55,6 +71,118 @@ export const reducer = (state = initialState, action) => {
       }
 
     case FETCH_CURRENT_USER_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      }
+
+    case POST_USER_LOGIN_START:
+      return {
+        ...state,
+        isLoading: true,
+        error: ''
+      }
+
+    case POST_USER_LOGIN_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        isLoggedIn: true,
+        error: '',
+        // window.localStorage.setItem('token', action.payload)
+      }
+
+    case POST_USER_LOGIN_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      }
+
+    case FETCH_USER_LOGOUT_START:
+      return {
+        ...state,
+        isLoading: true,
+        error: ''
+      }
+
+    case FETCH_USER_LOGOUT_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        isLoggedIn: false,
+        error: '',
+        // window.localStorage.removeItem('token')
+      }
+
+    case FETCH_USER_LOGOUT_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        isLoggedIn: true,
+        error: action.payload
+      }
+
+    case POST_USER_RECIPE_START:
+      return {
+        ...state,
+        isLoading: true,
+        error: ''
+      }
+
+    case POST_USER_RECIPE_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        newRecipeData: action.payload,
+        error: ''
+      }
+
+    case POST_USER_RECIPE_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      }
+
+    case DELETE_RECIPE_BY_ID_START:
+      return {
+        ...state,
+        isLoading: true,
+        error: ''
+      }
+
+    case DELETE_RECIPE_BY_ID_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        error: ''
+      }
+
+    case DELETE_RECIPE_BY_ID_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      }
+
+    case PUT_RECIPE_BY_ID_START:
+      return {
+        ...state,
+        isLoading: true,
+        error: ''
+      }
+
+    case PUT_RECIPE_BY_ID_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        newRecipeData: action.payload,
+        error: ''
+      }
+
+    case PUT_RECIPE_BY_ID_FAILURE:
       return {
         ...state,
         isLoading: false,
