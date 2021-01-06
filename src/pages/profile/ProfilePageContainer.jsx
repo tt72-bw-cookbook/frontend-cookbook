ad24896a6883db07e07ffd8e91cd0f958d56af2c
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-01-06 14:24:51 -0600

    refactored form

--- src/pages/profile/ProfilePageContainer.jsx
+++ src/pages/profile/ProfilePageContainer.jsx
@@ -11,37 +11,37 @@ const userURL = 'https://tt72-cookbook.herokuapp.com/users/current';
 
 const initialFormValues = {
 	// gen Info
-		title: '',
-		source: '',
+	title: '',
+	source: '',
 	// private check
-		private: true,
+	private: true,
 	// categories
-		categories: [{ 
-			course: '',
-			cuisine: '',
-			dietaryconcerns: '',
-			dishtype: '',
-			technique: '',
-			}],
+	categories: [{
+		course: '',
+		cuisine: '',
+		dietaryconcerns: '',
+		dishtype: '',
+		technique: '',
+	}],
 	// ingredients
-		ingredients: [{
-			ingredientname: '',
-			measurement: '',
-			quantity: 0, 
-			}],
+	ingredients: [{
+		ingredientname: '',
+		measurement: '',
+		quantity: 0,
+	}],
 	// instructions
-		instructions: '',
-  }
+	instructions: '',
+}
 
-  const initialFormErrors = {
+const initialFormErrors = {
 	title: '',
 	source: '',
 	ingredientname: '',
 	measurement: '',
 	instructions: '',
-  }
+}
 
-  const initialRecipes = [];
+const initialRecipes = [];
 
 
 
@@ -49,7 +49,7 @@ const ProfilePageContainer = props => {
 
 	const [user, setUser] = useState(null)
 	const [recipes, setRecipes] = useState(initialRecipes)
-	const [formValues, setFormValues] = useState([initialFormValues])
+	const [formValues, setFormValues] = useState(initialFormValues)
 	const [formErrors, setFormErrors] = useState(initialFormErrors)
 
 	useEffect(() => {
@@ -69,17 +69,17 @@ const ProfilePageContainer = props => {
 
 	const postNewRecipe = newRecipe => {
 		axios
-		.post('https://tt72-cookbook.herokuapp.com/recipes', newRecipe)
-		.then((res) => {
-		  setRecipes([res.data, ...recipes]);
-		  setFormValues(initialFormValues);
-		})
-		.catch((err) => {
-		  debugger;
-		})
-	  };
+			.post('https://tt72-cookbook.herokuapp.com/recipes', newRecipe)
+			.then((res) => {
+				setRecipes([res.data, ...recipes]);
+				setFormValues(initialFormValues);
+			})
+			.catch((err) => {
+				debugger;
+			})
+	};
 
-	  const inputChange = (name, value, index, event) => {
+	const inputChange = (name, value) => {
 
 		// Yup
 		//   .reach(schema, name) //get to this part of the schema
@@ -96,50 +96,58 @@ const ProfilePageContainer = props => {
 		// 	  [name]: err.errors[0],
 		// 	})
 		//   })
-	  
 
-		const values = [...formValues];
-		if (event.target.name === 'ingredientname') {
-			values[index].ingredientname = event.target.value;
-		}
-		else if (event.target.name === 'measurement') {
-			values[index].measurement = event.target.value;
-		}
+
+		// const values = [...formValues];
+		// if (event.target.name === 'ingredientname') {
+		// 	values[index].ingredientname = event.target.value;
+		// }
+		// else if (event.target.name === 'measurement') {
+		// 	values[index].measurement = event.target.value;
+		// }
+		// values
 		setFormValues({
-		  ...formValues,
-		  [name]: value,
-		  values
+			...formValues,
+			[name]: value,
 		})
-	  };
+	};
 
-	  const handleAddFields = () => {
-		const values = [...formValues];
-		values.push({ ingredientname: '', measurement: '' });
+	const ingredientChange = (name, value, index) => {
+		const values = { ...formValues }
+		values.ingredients[index][name] = value;
 		setFormValues(values);
-	  };
-	
-	  const handleRemoveFields = index => {
-		const values = [...formValues];
-		values.splice(index, 1);
-		setFormValues(values);
-	  };
+	}
 
-	  const formSubmit = () => {
+	const handleAddFields = () => {
+		// const values = [...formValues];
+		const values = { ...formValues };
+		values.ingredients.push({ ingredientname: '', measurement: '' });
+		setFormValues(values);
+	};
+
+	const handleRemoveFields = index => {
+		// const values = [...formValues];
+		const values = { ...formValues };
+		values.ingredients.splice(index, 1);
+		setFormValues(values);
+	};
+
+	const formSubmit = () => {
 		const newRecipe = {
-		  title: formValues.title.trim(),
-		  source: formValues.size.trim(),
-		  course: formValues.course.trim(),
-		  cuisine: formValues.cuisine.trim(),
-		  dietaryconcerns: formValues.dietaryconcerns.trim(),
-		  dishtype: formValues.dishtype.trim(),
-		  technique: formValues.technique.trim(),
-		  ingredientname: formValues.ingredientname.trim(),
-		  measurement: formValues.measurement.trim(),
-		  instructions: formValues.instructions.trim(),
-		  specialInstructions: formValues.specialInstructions.trim(),
+			title: formValues.title.trim(),
+			source: formValues.size.trim(),
+			course: formValues.course.trim(),
+			cuisine: formValues.cuisine.trim(),
+			dietaryconcerns: formValues.dietaryconcerns.trim(),
+			dishtype: formValues.dishtype.trim(),
+			technique: formValues.technique.trim(),
+			ingredientname: formValues.ingredientname.trim(),
+			measurement: formValues.measurement.trim(),
+			instructions: formValues.instructions.trim(),
+			specialInstructions: formValues.specialInstructions.trim(),
 		}
 		postNewRecipe(newRecipe);
-	  }
+	}
 
 
 
@@ -160,43 +168,44 @@ const ProfilePageContainer = props => {
 	return (
 		<>
 			<Header />
-				<ProfileBody>
-					<ProfileH2>Your Profile</ProfileH2>
-					<UserInfo>
-						<h1>{user.username}</h1>
-						<UserImg src={user ? user.profilepicture : "none"} />
-						<h3> Email: {user.email ?? "unknown"} </h3>
-						<h3> Since: {date ? date[0] : "unknown"}</h3>
-					</UserInfo>
-					<NewRecipeButton disabled={!user}> 
-						<Link to='./profile/add'>Add New Recipe</Link>
-					</NewRecipeButton>
-					<div>
-						<Switch>
-							<Route path='/profile/add'>
-								<AddRecipeForm
-									formValues={formValues}
-									change={inputChange}
-									errors={formErrors}
-									submit={formSubmit}
-									addField={handleAddFields}
-									remField={handleRemoveFields}
-								/>
-							</Route>
-						</Switch>
-					</div>
-					<ProfileH2>Your Recipes</ProfileH2>
-					<UserRecipes>
-						{
-							user?.recipes && user.recipes.map(userRecipes => {
-								return <ProfilePageRecipes key={userRecipes.userRecipeid} userRecipes={userRecipes} />;
-							})
-						}
-					</UserRecipes>
-					<div>
+			<ProfileBody>
+				<ProfileH2>Your Profile</ProfileH2>
+				<UserInfo>
+					<h1>{user.username}</h1>
+					<UserImg src={user ? user.profilepicture : "none"} />
+					<h3> Email: {user.email ?? "unknown"} </h3>
+					<h3> Since: {date ? date[0] : "unknown"}</h3>
+				</UserInfo>
+				<NewRecipeButton disabled={!user}>
+					<Link to='./profile/add'>Add New Recipe</Link>
+				</NewRecipeButton>
+				<div>
+					<Switch>
+						<Route path='/profile/add'>
+							<AddRecipeForm
+								formValues={formValues}
+								change={inputChange}
+								errors={formErrors}
+								submit={formSubmit}
+								ingredientChange={ingredientChange}
+								addField={handleAddFields}
+								remField={handleRemoveFields}
+							/>
+						</Route>
+					</Switch>
+				</div>
+				<ProfileH2>Your Recipes</ProfileH2>
+				<UserRecipes>
+					{
+						user?.recipes && user.recipes.map(userRecipes => {
+							return <ProfilePageRecipes key={userRecipes.userRecipeid} userRecipes={userRecipes} />;
+						})
+					}
+				</UserRecipes>
+				<div>
 
-					</div>
-				</ProfileBody>
+				</div>
+			</ProfileBody>
 		</>
 	)
 }
--- src/pages/profile/components/AddRecipeForm.jsx
+++ src/pages/profile/components/AddRecipeForm.jsx
@@ -1,98 +1,183 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, useEffect, Fragment } from "react";
+
+const AddIngredientFrag = ({ formValue, index, addField, remField, ingredientChange }) => {
+	// const  = props;
+
+	const [ingValues, setIngValues] = useState({ name: "", measurement: "", quantity: -1 })
+
+	useEffect(() => {
+		console.log(formValue)
+	}, [])
+
+	const onChange = (evt) => {
+		const { name, value } = evt.target;
+		ingredientChange(name, value, index)
+		// setIngValues({ ...ingValues, [name]: value });
+	}
+
+	return (
+		<>
+			<div>
+				<label htmlFor="ingredientname">Ingredient Name</label>
+				<input
+					onChange={onChange}
+					type="text"
+					className="form-control"
+					id="ingredientname"
+					name="ingredientname"
+					value={formValue.ingredientname}
+				// value={ingValues.name}
+				/>
+			</div>
+			<div>
+				<label htmlFor="measurement">Measurement</label>
+				<input
+					onChange={onChange}
+					type="text"
+					className="form-control"
+					id="measurement"
+					name="measurement"
+					value={formValue.measurement}
+				// value={ingValues.measurement}
+				/>
+			</div>
+			<div>
+				<label htmlFor="quantity">Quantity</label>
+				<input
+					onChange={onChange}
+					type="number"
+					className="form-control"
+					id="quantity"
+					name="quantity"
+					value={formValue.quantity}
+				// value={ingValues.quantity}
+				/>
+			</div>
+			<div>
+				<button
+					className="btn btn-link"
+					type="button"
+					onClick={() => remField(index)}
+				>
+					-
+							</button>
+				<button
+					className="btn btn-link"
+					type="button"
+					onClick={() => addField()}
+				>
+					+
+							</button>
+			</div>
+		</>
+	)
+}
 
 const AddRecipeForm = props => {
 
-// values, change, disabled, errors, submit
-const { 
-	formValues, 
-	change,
-	errors,
-	submit,
-	addField,
-	remField,
-} = props
+	// values, change, disabled, errors, submit
+	const {
+		formValues,
+		change,
+		errors,
+		submit,
+		addField,
+		remField,
+		ingredientChange
+	} = props
 
-const onChange = evt => {
-	const { name, value, type, checked } = evt.target
-	const valueToUse = type === 'checkbox' ? checked : value;
-	change(name, valueToUse);
-  }
+	const onChange = evt => {
+		const { name, value, type, checked } = evt.target
+		const valueToUse = type === 'checkbox' ? checked : value;
+		change(name, valueToUse);
+	}
 
-// const onSubmit = (evt) => {
-// 	evt.preventDefault()
-// 	submit()
-// }
+	// const onSubmit = (evt) => {
+	// 	evt.preventDefault()
+	// 	submit()
+	// }
 
 
 	return (
 		<>
-		<div>
-			<h1>Add Recipe Form</h1>
-			<form>
-				<div>
-					<label>Recipe Name:
+			<div>
+				<h1>Add Recipe Form</h1>
+				<form>
+					<div>
+						<label>Recipe Name:
 						<input
-							onChange={onChange}
-							value={formValues.title}
-							name='title'
-							type='text'
-						/>
-					</label>
-					<label>Recipe Source:
+								onChange={onChange}
+								value={formValues.title}
+								name='title'
+								type='text'
+							/>
+						</label>
+						<label>Recipe Source:
 						<input
-							onChange={onChange}
-							value={formValues.source}
-							name='source'
-							type='text'
-						/>
-					</label>
-					<label>Private?
-                    <input  type='checkbox' name='private' checked={formValues.private} onChange={onChange}/>
-                    </label>
-				</div>
-				<div>
-					<label>Course:
+								onChange={onChange}
+								value={formValues.source}
+								name='source'
+								type='text'
+							/>
+						</label>
+						<label>Private?
+                    <input type='checkbox' name='private' checked={formValues.private} onChange={onChange} />
+						</label>
+					</div>
+					<div>
+						{/* <label>Course:
 						<input
-							onChange={onChange}
-							value={formValues.course}
-							name='course'
-							type='text'
-						/>
-					</label>
-					<label>Cuisine:
+								onChange={onChange}
+								value={formValues.course}
+								name='course'
+								type='text'
+							/>
+						</label> */}
+						<select id="course" name="course" onChange={onChange}>
+							<option selected value="null">--pick a course--</option>
+							<option value="breakfast">Breakfast</option>
+							<option value="lunch">lunch</option>
+							<option value="dinner">dinner</option>
+							<option value="drink">drink</option>
+							<option value="snack">snack</option>
+						</select>
+
+						{/* <optgroup>
+						</optgroup> */}
+						<label>Cuisine:
 						<input
-							onChange={onChange}
-							value={formValues.cuisine}
-							name='cuisine'
-							type='text'
-						/>
-					</label>
-					<label>Dietary Concerns:
+								onChange={onChange}
+								value={formValues.cuisine}
+								name='cuisine'
+								type='text'
+							/>
+						</label>
+						<label>Dietary Concerns:
 						<input
-							onChange={onChange}
-							value={formValues.dietaryconcerns}
-							name='dietaryconcerns'
-							type='text'
-						/>
-					</label>
-					<label>Type of Dish:
+								onChange={onChange}
+								value={formValues.dietaryconcerns}
+								name='dietaryconcerns'
+								type='text'
+							/>
+						</label>
+						<label>Type of Dish:
 						<input
-							onChange={onChange}
-							value={formValues.dishtype}
-							name='dishtype'
-							type='text'
-						/>
-					</label>
-					<label>Technique:
+								onChange={onChange}
+								value={formValues.dishtype}
+								name='dishtype'
+								type='text'
+							/>
+						</label>
+						<label>Technique:
 						<input
-							onChange={onChange}
-							value={formValues.technique}
-							name='technique'
-							type='text'
-						/>
-					</label>
-				</div>
-				{/* <div>
+								onChange={onChange}
+								value={formValues.technique}
+								name='technique'
+								type='text'
+							/>
+						</label>
+					</div>
+					{/* <div>
 					<label> Ingredient Name:
 						<input
 							onChange={onChange}
@@ -110,63 +195,71 @@ const onChange = evt => {
 						/>
 					</label>
 				</div> */}
-				<div className="form-row">
-					{formValues.map((formValue, index) => (
-						<Fragment key={`${formValue}~${index}`}>
-						<div>
-							<label htmlFor="ingredientname">Ingredient Name</label>
-							<input
-							onChange={onChange}
-							type="text"
-							className="form-control"
-							id="ingredientname"
-							name="ingredientname"
-							value={formValue.ingredientname}
+					<div className="form-row">
+						{formValues.ingredients.map((formValue, index) => (
+							<AddIngredientFrag
+								ingredientChange={ingredientChange}
+								key={`${formValue}~${index}`}
+								formValue={formValue}
+								index={index}
+								remField={remField}
+								addField={addField}
 							/>
-						</div>
-						<div>
-							<label htmlFor="measurement">Measurement</label>
-							<input
-							onChange={onChange}
-							type="text"
-							className="form-control"
-							id="measurement"
-							name="measurement"
-							value={formValue.measurement}
-							/>
-						</div>
-						<div>
-							<button
-							className="btn btn-link"
-							type="button"
-							onClick={() => remField(index)}
-							>
-							-
-							</button>
-							<button
-							className="btn btn-link"
-							type="button"
-							onClick={() => addField()}
-							>
-							+
-							</button>
-						</div>
-						</Fragment>
-					))}
-				</div>
-				<div>
-					<label> Instructions:
+							// <Fragment key={`${formValue}~${index}`}>
+							// 	<div>
+							// 		<label htmlFor="ingredientname">Ingredient Name</label>
+							// 		<input
+							// 			onChange={onChange}
+							// 			type="text"
+							// 			className="form-control"
+							// 			id="ingredientname"
+							// 			name="ingredientname"
+							// 			value={formValue.ingredientname}
+							// 		/>
+							// 	</div>
+							// 	<div>
+							// 		<label htmlFor="measurement">Measurement</label>
+							// 		<input
+							// 			onChange={onChange}
+							// 			type="text"
+							// 			className="form-control"
+							// 			id="measurement"
+							// 			name="measurement"
+							// 			value={formValue.measurement}
+							// 		/>
+							// 	</div>
+							// 	<div>
+							// 		<button
+							// 			className="btn btn-link"
+							// 			type="button"
+							// 			onClick={() => remField(index)}
+							// 		>
+							// 			-
+							// </button>
+							// 		<button
+							// 			className="btn btn-link"
+							// 			type="button"
+							// 			onClick={() => addField()}
+							// 		>
+							// 			+
+							// </button>
+							// 	</div>
+							// </Fragment>
+						))}
+					</div>
+					<div>
+						<label> Instructions:
 						<input
-							onChange={onChange}
-							value={formValues.instructions}
-							name='instructions'
-							type='text'
-						/>
-					</label>
-				</div>
-				<button className='submitButton'>Submit Recipe</button>
-			</form>
-		</div>
+								onChange={onChange}
+								value={formValues.instructions}
+								name='instructions'
+								type='text'
+							/>
+						</label>
+					</div>
+					<button className='submitButton'>Submit Recipe</button>
+				</form>
+			</div>
 		</>
 	);
 };
