56ea8901b9d92d800239cbc1d27a63e71a139d5a
reesharper <reestimharper@gmail.com>
2021-01-06 18:40:22 -0500

    started adding signup redux

--- src/store/vanillaRedux/reducers/index.js
+++ src/store/vanillaRedux/reducers/index.js
@@ -21,7 +21,10 @@ import {
 	PUT_RECIPE_BY_ID_SUCCESS,
   PUT_RECIPE_BY_ID_FAILURE,
   REJECT_LOGGED_IN,
-  CONFIRM_LOGGED_IN
+  CONFIRM_LOGGED_IN,
+  POST_NEW_USER_START,
+  POST_NEW_USER_SUCCESS,
+  POST_NEW_USER_FAILURE
 } from '../actions/index'
 
 const initialState = {
@@ -197,6 +200,7 @@ export const vanillaReducer = (state = initialState, action) => {
 				error: action.payload
       }
       
+    //login authentication
     case CONFIRM_LOGGED_IN:
       return {
         ...state,
@@ -207,6 +211,22 @@ export const vanillaReducer = (state = initialState, action) => {
       return {
         ...state,
         isLoggedIn: action.payload
+      }
+
+    //signup
+    case POST_NEW_USER_START:
+      return {
+
+      }
+
+    case POST_NEW_USER_SUCCESS:
+      return {
+
+      }
+
+    case POST_NEW_USER_FAILURE:
+      return {
+        
       }
 
 		default:
