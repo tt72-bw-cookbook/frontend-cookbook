d07684a3c9b60f5f18a15eb27cd94071210dfeb5
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-01-07 20:44:11 -0600

    delete empty archive files, comment out non-empty archive files

--- src/archive/EditRecipeForm.jsx
+++ src/archive/EditRecipeForm.jsx
@@ -1,9 +1,9 @@
-const EditRecipeForm = props => {
-	return (
-		<div>
-			<h1>Edit Recipe Form</h1>
-		</div>
-	)
-}
+// const EditRecipeForm = props => {
+// 	return (
+// 		<div>
+// 			<h1>Edit Recipe Form</h1>
+// 		</div>
+// 	)
+// }
 
-export default EditRecipeForm;
+// export default EditRecipeForm;
--- src/pages/homepage/components/index.js
+++ src/pages/homepage/components/index.js
@@ -1,3 +1,3 @@
-export { default as HomePageFilterForm } from "./HomePageFilterForm";
+export { default as HomePageFilterForm } from "../../../archive/HomePageFilterForm";
 export { default as HomePageRecipeCard } from "./homePageRecipeCard";
 export { default as HomePageRecipes } from "./homePageRecipes";
