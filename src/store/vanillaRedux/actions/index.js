f6431ef8c4f1fe55365140f105f64b7c7b1378de
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-01-10 22:32:46 -0500

    edit form working

--- src/store/vanillaRedux/actions/index.js
+++ src/store/vanillaRedux/actions/index.js
@@ -197,7 +197,7 @@ export const deleteRecipeById = (id) => {
 export const putRecipeById = (id, updateFields) => {
 	return (dispatch) => {
 		dispatch({ type: PUT_RECIPE_BY_ID_START });
-		axiosAuth().put(`recipe/${id}`, updateFields)
+		axiosAuth().patch(`recipes/${id}`, updateFields)
 			.then((res) => {
 				dispatch({
 					type: PUT_RECIPE_BY_ID_SUCCESS,
